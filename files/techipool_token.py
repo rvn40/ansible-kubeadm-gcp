9646f0af9df990b2e408a2697155e008ee3f089c
rivan <rivan.pratama@amarbank.co.id>
2023-04-29 14:41:36 +0700

    Update: Add python token generator and update the task

---
+++ files/techipool_token.py
@@ -0,0 +1,12 @@
+from google.auth.transport import requests
+from google.oauth2 import service_account
+
+CREDENTIAL_SCOPES = ["https://www.googleapis.com/auth/cloud-platform"]
+CREDENTIALS_KEY_PATH = "serviceaccount.pem"
+
+def get_service_account_token():
+  credentials = service_account.Credentials.from_service_account_file(CREDENTIALS_KEY_PATH, scopes=CREDENTIAL_SCOPES)
+  credentials.refresh(requests.Request())
+  return credentials.token
+
+print(get_service_account_token())
