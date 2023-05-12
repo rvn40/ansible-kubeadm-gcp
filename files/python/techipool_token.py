02237d3a3082bb7dadcabb70ce4a1ce45ff716fd
rivan <rivan.pratama@amarbank.co.id>
2023-05-12 18:12:27 +0700

    Update: Update for optimizing scripts

--- files/python/techipool_token.py
+++ files/python/techipool_token.py
@@ -2,7 +2,7 @@ from google.auth.transport import requests
 from google.oauth2 import service_account
 
 CREDENTIAL_SCOPES = ["https://www.googleapis.com/auth/cloud-platform"]
-CREDENTIALS_KEY_PATH = "files/serviceaccounts/techipool.pem"
+CREDENTIALS_KEY_PATH = "files/serviceaccounts/abdvp.pem"
 
 def get_service_account_token():
   credentials = service_account.Credentials.from_service_account_file(CREDENTIALS_KEY_PATH, scopes=CREDENTIAL_SCOPES)
